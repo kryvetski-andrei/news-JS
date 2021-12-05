798eef8fef21342af5bbd2483c2b3638dcb8566f
kryvetski-andrei <kryvetskistud@gmail.com>
2021-12-05 20:22:42 +0300

    fix convert news.js to news.ts

--- src/components/view/appView.ts
+++ src/components/view/appView.ts
@@ -10,9 +10,10 @@ export class AppView {
         this.sources = new Sources();
     }
 
-    drawNews(data: {status: string, totalResults: number, articles: Array<object>}) { //{}
+    drawNews(data: {status: string, totalResults: number, articles: {author: string, content: string, description: string, publishedAt: string,  source: {id: string, name: string}, title: string, url: string, urlToImage: string}[]}) { //{}
         //data: {status: 'ok', totalResults: 3538, articles: Array(20)} 
-        const values = data?.articles  ? data?.articles : []; 
+        const values = data?.articles ? data?.articles : []; 
+        console.log(data.articles, 'data.articles')
         this.news.draw(values);  //values: (20) [{…}, {…}, {…}, {…}...]
     }
 
---
+++ src/components/view/news/news.ts
@@ -0,0 +1,57 @@
+import './news.css';
+
+class News {
+    draw(data: {author: string, content: string, description: string, publishedAt: string,  source: {id: string, name: string}, title: string, url: string, urlToImage: string}[]) {
+        console.log(data, 'data from News') //(20) [{…}, {…}, {…}, {…}, {…}, {…}, {…}, {…}, {…}, {…}, {…}, {…}, {…}, {…}, {…}, {…}, {…}, {…}, {…}, {…}]
+        const news = data.length >= 10 ? data.filter((_item, idx) => idx < 10) : data;
+        console.log(news, 'news from News') //(10) [{…}, {…}, {…}, {…}, {…}, {…}, {…}, {…}, {…}, {…}] 
+        
+        const fragment = document.createDocumentFragment();
+        const newsItemTemp = <HTMLTemplateElement>document.querySelector('#newsItemTemp');
+        
+
+        console.log(fragment, 'fragment document.createDocumentFragment() from News') //#document-fragment
+        console.log(newsItemTemp, 'newsItemTemp document.querySelector(#newsItemTemp) from News') //<template id="newsItemTemp"></template>
+
+        news.forEach((item, idx) => {
+
+            /** item
+              0:
+                author: "Simon Zetlitz Nessler, VG"
+                content: "(Manchester United Crystal Palace 10) Manchester Uniteds midlertidige manager Ralf Rangnick (63) kunne juble for tre poeng i sin første kamp i sjefsstolen. \r\nSist oppdatert nå nettopp\r\nJUBEL: Fred om… [+2238 chars]"
+                description: "(Manchester United – Crystal Palace 1–0) Manchester Uniteds midlertidige manager Ralf Rangnick (63) kunne juble for tre poeng i sin første kamp i sjefsstolen."
+                publishedAt: "2021-12-05T15:54:20Z"
+                source: {id: 'aftenposten', name: 'Aftenposten'}
+                title: "Perlemål ga Rangnick perfekt start i Manchester United"
+                url: "https://www.aftenposten.no/sport/fotball/i/a7qvEL/freds-perlemaal-ga-rangnick-perfekt-start-i-manchester-united"
+                urlToImage: "https://premium.vgc.no/v2/images/fc6da2bd-950f-4ec3-bbdc-1f8e7e5fb3dd?fit=crop&format  
+            */
+                
+            const newsClone = <HTMLElement>newsItemTemp.content.cloneNode(true);
+            console.log(typeof newsClone, 'newsClone newsItemTemp.content.cloneNode(true) from News ') //object #document-fragment
+            if (idx % 2) (<HTMLElement>newsClone.querySelector('.news__item')).classList.add('alt');
+
+            (<HTMLElement>newsClone.querySelector('.news__meta-photo')).style.backgroundImage = `url(${
+                item.urlToImage || 'img/news_placeholder.jpg'
+            })`;
+            (<HTMLElement>newsClone.querySelector('.news__meta-author')).textContent = item.author || item.source.name;
+            (<HTMLElement>newsClone.querySelector('.news__meta-date')).textContent = item.publishedAt
+                .slice(0, 10)
+                .split('-')
+                .reverse()
+                .join('-');
+
+            (<HTMLElement>newsClone.querySelector('.news__description-title')).textContent = item.title;
+            (<HTMLElement>newsClone.querySelector('.news__description-source')).textContent = item.source.name;
+            (<HTMLElement>newsClone.querySelector('.news__description-content')).textContent = item.description;
+            (<HTMLElement>newsClone.querySelector('.news__read-more a')).setAttribute('href', item.url);
+
+            fragment.append(newsClone);
+        });
+
+        (<HTMLElement>document.querySelector('.news')).innerHTML = '';
+        (<HTMLElement>document.querySelector('.news')).appendChild(fragment);
+    }
+}
+
+export default News;
