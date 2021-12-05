3059a042aed9c986e094e9e9b68cfac23b5da19e
kryvetski-andrei <kryvetskistud@gmail.com>
2021-12-05 10:53:55 +0300

    init: start migration-to-typescript task

--- src/components/controller/appLoader.js
+++ src/components/controller/appLoader.js
@@ -3,7 +3,7 @@ import Loader from './loader';
 class AppLoader extends Loader {
     constructor() {
         super('https://newsapi.org/v2/', {
-            apiKey: '', // получите свой ключ https://newsapi.org/
+            apiKey: 'da321c78ab6b48518e38b78758b2dbb6',
         });
     }
 }
