f003584e290d6e741fdb6b53aa6f69a5d3d1ef12
kryvetski-andrei <kryvetskistud@gmail.com>
2021-12-05 19:19:46 +0300

    fix: convert appView.js to appView.ts

---
+++ src/components/view/appView.ts
@@ -0,0 +1,26 @@
+import News from './news/news';
+import Sources from './sources/sources';
+
+export class AppView {
+    news: News;
+    sources: Sources;
+
+    constructor() {
+        this.news = new News();
+        this.sources = new Sources();
+    }
+
+    drawNews(data: {status: string, totalResults: number, articles: Array<object>}) { //{}
+        //data: {status: 'ok', totalResults: 3538, articles: Array(20)} 
+        const values = data?.articles  ? data?.articles : []; 
+        this.news.draw(values);  //values: (20) [{…}, {…}, {…}, {…}...]
+    }
+
+    drawSources(data: {status: string, sources: Array<object>}) {
+        //data: {status: 'ok', sources: Array(128)}
+        const values = data?.sources ? data?.sources : [];
+        this.sources.draw(values);  //values: (128) [{…}, {…}, {…}, {…}...]
+    }
+}
+
+export default AppView;
