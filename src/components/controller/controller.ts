ff442e63b108e6c91ce3349c06e69abe395593fb
kryvetski-andrei <kryvetskistud@gmail.com>
2021-12-05 23:43:18 +0300

    fix: convert controller.js to controller.ts

--- src/components/controller/controller.ts
+++ src/components/controller/controller.ts
@@ -1,7 +1,7 @@
 import AppLoader from './appLoader';
 
 class AppController extends AppLoader {
-    getSources(callback) {
+    getSources(callback: (() => void)) {
         super.getResp(
             {
                 endpoint: 'sources',
@@ -10,13 +10,14 @@ class AppController extends AppLoader {
         );
     }
 
-    getNews(e, callback) {
-        let target = e.target;
-        const newsContainer = e.currentTarget;
+    getNews(e: Event, callback: (() => void)) {
+        let target = <HTMLElement>e.target;
+        const newsContainer = <HTMLElement>e.currentTarget;
 
         while (target !== newsContainer) {
             if (target.classList.contains('source__item')) {
-                const sourceId = target.getAttribute('data-source-id');
+                const sourceId = <string>target.getAttribute('data-source-id');
+                console.log(typeof sourceId, 'soooooooooooooooouscceIIIIIIIIIDDDDD')
                 if (newsContainer.getAttribute('data-source') !== sourceId) {
                     newsContainer.setAttribute('data-source', sourceId);
                     super.getResp(
@@ -31,7 +32,7 @@ class AppController extends AppLoader {
                 }
                 return;
             }
-            target = target.parentNode;
+            target = target.parentNode as HTMLElement;
         }
     }
 }
